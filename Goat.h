13f79f567d4a393e4755e38114bed8c665158bef
Avleen Kaur <avleen@Avleens-Air.attlocal.net>
2024-10-27 16:02:00 -0700

    second

--- Goat.h
+++ Goat.h
@@ -11,9 +11,13 @@ private:
     int age;
     string color;
 public: 
-    Goat()                          { name = ""; age = 0; color = ""; }
+    Goat()                             { name = ""; age = 0; color = ""; }
     // write three more constructors
-    
+    Goat(string n)                     { name = n; age = 0; color = ""; } 
+    Goat(string n, int a)              { name = n; age = a; color = ""; }
+    Goat(string n, int a, string c)    { name = n; }
+
+
     // setters and getters
     void set_name(string n)         { name = n; };
     string get_name() const         { return name; };
